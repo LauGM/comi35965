d6a472ca1d8ab94eee85c4e1598d0ead60b72888
Lau G.M <lmartin@animac.com.ar>
2022-11-28 22:32:49 -0300

    clase 5

---
+++ clase5/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h3 class="text-primary">{{ msg }}</h3>
+    <button class="btn btn-primary">
+     Hola Bootstrap
+   </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
